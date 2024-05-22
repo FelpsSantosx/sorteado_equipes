cdd9a1b9fa199955c3cc0ca0cea50e276a8e4ee0
FelpsSantosx <lipesouzasanto@gmail.com>
2024-05-21 21:22:23 -0300

    arquitetura fail

---
+++ components/Home/home.tsx
@@ -0,0 +1,12 @@
+import { View, Text, ViewBase } from "react-native";
+
+
+import { styles } from "./style";
+
+export function Home(){
+    return (
+        <View style={styles.container}>
+            <Text>Teste</Text>
+        </View>
+    )
+}
