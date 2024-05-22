3451934695cc3eb7aa84b48f40fdd65f5e348b8f
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-05-21 22:06:13 -0300

    Botao começar

--- components/Home/home.tsx
+++ components/Home/home.tsx
@@ -1,12 +1,24 @@
-import { View, Text, ViewBase } from "react-native";
+import { View, Text, ViewBase, Button } from "react-native";
 
 
 import { styles } from "./style";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export function Home(){
     return (
-        <View style={styles.container}>
-            <Text>Teste</Text>
-        </View>
+        <>
+            <View style={styles.container}>
+                <Text>Teste</Text>
+                    
+            </View>
+
+            <View style={styles.botao}>
+                <Button 
+                    color={'#000'}
+                    onPress={Header}
+                    title="Click me!" 
+                />
+            </View>
+        </>
     )
 }
